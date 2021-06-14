1132bd9855e316c4deeae01509bdeee1442dca20
Zachary Walker <w.zacharyk@gmail.com>
2021-06-14 19:43:15 +0100

    query update

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,9 +1,9 @@
 export const API_BASE_URL = 'http://localhost:4000';
 
 export const API_TOURNAMENTS_URL_QUERY = (q?: string) =>
-  `${API_BASE_URL}/tournaments/?q=${q}`;
+  `${API_BASE_URL}/tournaments/?q=${q?.trim().replace(' ', '+')}`;
 
 export const API_TOURNAMENTS_URL = `${API_BASE_URL}/tournaments`;
 
-export const API_TOURNAMENTS_PATCH_URL = (id: string) =>
+export const API_TOURNAMENTS_URL_PATCH = (id: string) =>
   `${API_TOURNAMENTS_URL}/${id}`;
